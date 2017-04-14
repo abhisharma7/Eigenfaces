a8be683ba45581b17d261c10ca308ba5de9ab87b
Abhishek <abhisharma2588@gmail.com>
2017-04-14 00:17:54 -0500

    Grid FUnction

--- pca_cal.py
+++ pca_cal.py
@@ -9,6 +9,23 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
 
+def display_grid(images):
+
+    grid = gridspec.GridSpec(5, 5, top=1., bottom=0., right=1., left=0., hspace=0., wspace=0.)
+    count = 0
+    dim = (100,100)
+    for g in grid:
+        ax = plt.subplot(g)
+        resized = cv2.resize(images[count], dim, interpolation= cv2.INTER_AREA)
+        count = count + 1
+        ax.imshow(resized)
+        ax.set_xticks([])
+        ax.set_yticks([])
+    plt.show()
+                                    
+
+
+
 train_images_path = "Eigenfaces/Train/"
 train_path_list = []
 for files in os.listdir(train_images_path):
@@ -19,23 +36,22 @@ for files in os.listdir(train_images_path):
 images = np.empty(len(train_path_list),dtype=object)
 
 for n in range(0, len(train_path_list)):
-    images[n] = cv2.imread(os.path.join(train_images_path,train_path_list[n]))#,cv2.COLOR_BGR2GRAY)#cv2.IMREAD_GRAYSCALE)
-   
+    images[n] = cv2.imread(os.path.join(train_images_path,train_path_list[n]))
 
-grid = gridspec.GridSpec(5, 5, top=1., bottom=0., right=1., left=0., hspace=0., wspace=0.)
-count = 0
-dim = (100,100)
-for g in grid:
-    ax = plt.subplot(g)
-    resized = cv2.resize(images[count], dim, interpolation= cv2.INTER_AREA)
-    count = count + 1
-    ax.imshow(resized)
-    ax.set_xticks([])
-    ax.set_yticks([])
-plt.show()
+display_grid(images)
+
+
+
+#grid = gridspec.GridSpec(5, 5, top=1., bottom=0., right=1., left=0., hspace=0., wspace=0.)
+#count = 0
+#dim = (100,100)
+#for g in grid:
+#    ax = plt.subplot(g)
+#    resized = cv2.resize(images[count], dim, interpolation= cv2.INTER_AREA)
+#    count = count + 1
+#    ax.imshow(resized)
+#    ax.set_xticks([])
+#    ax.set_yticks([])
+#plt.show()
     
-#cv2.imshow("resize",resized)
-#cv2.imshow("original",images[0])
-#cv2.waitKey(0)
-
 
