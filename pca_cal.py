d9eb90cf99436b530d941ffdb1ebef3717779e38
Abhishek <abhisharma2588@gmail.com>
2017-04-15 16:01:16 -0500

    Images Read and Display: Class Structure

--- pca_cal.py
+++ pca_cal.py
@@ -1,40 +1,53 @@
 #!/home/hackpython/anaconda3/bin/python
 
 # Author: Abhishek Sharma
+# Program: Eigenfaces 
 
 import os
-import cv2
 import numpy as np
+from scipy.misc import * 
+from scipy import linalg
+import glob
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+import cv2
+
+class Eigenfaces:
+
+    def __init__(self):
+        self.traindir = 'Eigenfaces/Train/'
+        self.testdir  =  'Eigenfaces/Test/'
+        self.images_path = []
+        self.displaygrid = True
+        self.main_function()
+    
+    def main_function(self):
+            
+        images_array = self.load_images()
+    
+        if self.displaygrid:
+            self.display_images(100,100)
+
+    def load_images(self):
+        
+        self.images_path = glob.glob(self.traindir + '/*.jpg')
+        images_array = np.array([imread(i,True).flatten() for i in self.images_path])        
+        return images_array
+
+    def display_images(self,width,height):
+
+        grid = gridspec.GridSpec(5, 5, top=1., bottom=0., right=1., left=0., hspace=0., wspace=0.)
+        count = 0
+        dim = (width,height)
+        for g in grid:
+            ax = plt.subplot(g)
+            resized = cv2.resize(cv2.imread(self.images_path[count]), dim, interpolation= cv2.INTER_AREA)
+            count = count + 1
+            ax.imshow(resized)
+            ax.set_xticks([])
+            ax.set_yticks([])
+        plt.show()
 
 
-def display_grid(images):
-    grid = gridspec.GridSpec(5, 5, top=1., bottom=0., right=1., left=0., hspace=0., wspace=0.)
-    count = 0
-    dim = (100,100)
-    for g in grid:
-        ax = plt.subplot(g)
-        resized = cv2.resize(images[count], dim, interpolation= cv2.INTER_AREA)
-        count = count + 1
-        ax.imshow(resized)
-        ax.set_xticks([])
-        ax.set_yticks([])
-    plt.show()
-                                    
-train_images_path = "Eigenfaces/Train/"
-train_path_list = []
-for files in os.listdir(train_images_path):
-    if os.path.isfile(os.path.join(train_images_path,files)):
-        train_path_list.append(files)
-
-# np.empty:Return a new array of given shape and type, without initializing entries. 
-images = np.empty(len(train_path_list),dtype=object)
-
-for n in range(0, len(train_path_list)):
-    images[n] = cv2.imread(os.path.join(train_images_path,train_path_list[n]))
-
-#print(np.ravel(images[0]))
-#display_grid(images)
-
-
+if __name__ == '__main__':
+    Eigenfaces()
